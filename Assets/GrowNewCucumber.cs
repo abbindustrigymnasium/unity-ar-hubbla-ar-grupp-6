b9a5901bb487a01bcdd2ce3f41d396f9d13d7dfb
abbseblin <sebastian.ling@abbgymnasiet.se>
2023-03-24 14:19:23 +0100

    gurk räknare och början av tidräkning

--- Assets/GrowNewCucumber.cs
+++ Assets/GrowNewCucumber.cs
@@ -8,32 +8,16 @@ public class GrowNewCucumber : MonoBehaviour
 {
     [SerializeField] private GameObject Gurka;
     private int timesGrown;
-    public TextMeshProUGUI NoWater;
-
     private void Start()
     {
         timesGrown = 0;
-        NoWater.text = "";
     }
 
     private void OnMouseDown()
     {
-        if (timesGrown < 3)
-        {
-            Gurka.SetActive(true);
-            timesGrown++;
-        }
-        else
-        {
-            NoWater.text = "Slut På Vatten!";
-            StartCoroutine(Countdown());
-        }
-        Debug.Log(timesGrown);
-    }
+        Gurka.SetActive(true);
+        timesGrown++;
 
-    private IEnumerator Countdown()
-    {
-        yield return new WaitForSeconds(1.5f); 
-        NoWater.text = "";
+        Debug.Log(timesGrown);
     }
 }
--- Assets/Scripts/finishEating.cs
+++ Assets/Scripts/finishEating.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class finishEating : MonoBehaviour
 {
     [SerializeField] private GameObject WateringCan;
+    public TextMeshProUGUI CucumberText;
+    private int CucumberInt;
+
+
+    private void Start()
+    {
+        CucumberInt = 0;
+    }
+
     private void OnMouseDown()
     {
+        CucumberInt++;
+        GurkaIntAdd(CucumberInt.ToString());
         gameObject.SetActive(false);
         Debug.Log("YUMMY!");
         WateringCan.SetActive(true);
+    }
+
+    public void GurkaIntAdd(string num)
+    {
+        num = CucumberInt.ToString();
+        CucumberText.text = num;
     }
 }
